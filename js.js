36c904f87c3ea2e2434ab68fbf5ee48b37b0cc3e
Tien <ldtien.1993@gmail.com>
2021-12-29 19:53:11 +0700

    add mo hinh 3 khoi" git push

--- js.js
+++ js.js
@@ -1,4 +1,11 @@
 // bai 1
+// Khối 1: Nhập vào 3 số nguyên
+// khối 2:
+//     -Bước 1: tạo biến num1, 2, 3;
+//     -Bước 2: người dùng nhập số nguyên và gán vào biến num;
+//     -Bước 3: tạo thêm biến tạm thời để so sánh các biến num người dùng nhập, swap các biến để num 1 là nhỏ nhất và num3 là lớn nhất
+//     -Bước 4: Xuất kết quả
+// Khối 3:Kiểm tra số có hợp lệ không, in ra kết quả cho người dùng
 
 var num1El = document.getElementById("num1");
 var num2El = document.getElementById("num2");
@@ -30,6 +37,14 @@ function compareNumber() {
 }
 
 // bai 2
+// Khối 1: Nhập vào tên người dùng
+// khối 2:
+//     -Bước 1: tạo biến role
+//     -Bước 2: người dùng tên và gán vào biến role;
+//     -Bước 3: so sánh tên người dùng nhập với các trường hợp có sẵn: bố, mẹ, anh trai, em gái
+//     -Bước 4: Xuất kết quả
+// Khối 3:Kiểm tra người dùng nhập đúng tên, nếu không sẽ hướng dẫn nhập lại, in ra kết quả cho người dùng
+
 var roleEl = document.getElementById("role");
 var roleTxtEl = document.getElementById("roleTxt");
 
@@ -55,6 +70,14 @@ function enterName() {
 }
 
 //bai 3
+// Khối 1: Nhập vào 3 số nguyên
+// khối 2:
+//     -Bước 1: tạo biến num4, 5, 6;
+//     -Bước 2: người dùng nhập số nguyên và gán vào biến num;
+//     -Bước 3: Xét các trường hợp biến num chia có dư và không có dư => tính tổng
+//     -Bước 4: Xuất kết quả
+// Khối 3:Kiểm tra số có hợp lệ không, in ra kết quả cho người dùng
+
 var num4El = document.getElementById("num4");
 var num5El = document.getElementById("num5");
 var num6El = document.getElementById("num6");
@@ -90,6 +113,13 @@ function evenOrOdd() {
 }
 
 // bai 4
+// Khối 1: Nhập vào 3 cạnh tam giác
+// khối 2:
+//     -Bước 1: tạo biến chứa 3 cạnh tam giác canhA, canhB, canhC
+//     -Bước 2: người dùng nhập vào 3 cạnh tam giác và gán vào biến
+//     -Bước 3: So sánh các trường hợp các cạnh tam giác: loại trừ trước trường hợp tam giác không hợp lệ, sau đó xét đến tam giác đều 3 cạnh = nhau, rồi xét đến tam giác vuông và cân, cuối cùng là tam giác thường.
+//     -Bước 4: Xuất kết quả
+// Khối 3:Kiểm tra số có hợp lệ không, in ra kết quả cho người dùng
 
 var canhAEl = document.getElementById("canhA");
 var canhBEl = document.getElementById("canhB");
